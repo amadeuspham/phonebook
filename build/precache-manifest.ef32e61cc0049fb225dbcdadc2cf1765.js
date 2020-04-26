76763af01f878259209f34f01b275b94c970b83c
hunganh <scramblegf@gmail.com>
2020-04-26 16:33:06 +0300

    Add front-end build

---
+++ build/precache-manifest.ef32e61cc0049fb225dbcdadc2cf1765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bf46e067e620579a729f2df98a6e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "552b35ed07bab8d2c663",
+    "url": "/static/css/main.a76cede8.chunk.css"
+  },
+  {
+    "revision": "c5cf8bd149580aa910cd",
+    "url": "/static/js/2.57bf6790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57bf6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552b35ed07bab8d2c663",
+    "url": "/static/js/main.b8d8b292.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
